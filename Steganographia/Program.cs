a0d3bdfd602aa33957b006d3402a4be5c88165e3
Sergorila <64915221+Sergorila@users.noreply.github.com>
2022-10-11 13:26:47 +0400

    Add files via upload

---
+++ Steganographia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpath = @"C:\Users\sidorovsa\source\repos\Steganographia\Steganographia\container.txt";
+            string spath = @"C:\Users\sidorovsa\source\repos\Steganographia\Steganographia\stegocontainer.txt";
+            Writer w = new Writer("a", cpath, spath);
+            w.Coding();
+        }
+    }
+}
---
+++ Steganographia/Writer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coder
+{
+    public class Writer
+    {
+        private string _message;
+        private string _containerPath;
+        private string _stegocontainerPath;
+
+        public Writer(string message, string containerPath, string stegocontainerPath)
+        {
+            _message = message;
+            _containerPath = containerPath;
+            _stegocontainerPath = stegocontainerPath;
+        }
+
+        public void Coding()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in System.Text.Encoding.Unicode.GetBytes("amogus"))
+                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+
+            string binaryStr = sb.ToString();
+            Console.WriteLine(binaryStr);
+
+            var stringArray = Enumerable.Range(0, binaryStr.Length / 8).Select(i => Convert.ToByte(binaryStr.Substring(i * 8, 8), 2)).ToArray();
+            var str = Encoding.UTF8.GetString(stringArray);
+            Console.WriteLine(str);
+        }
+    }
+}
